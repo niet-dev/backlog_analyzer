8fab139c5dcb89d4a9203ed0cd61cac519b7c7f5
Matt Long <nietscape@gmail.com>
2026-01-29 12:23:28 -0800

    tests for column mapping

---
+++ data/mappings.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class ColumnMapping:
+    source_names: list[str]
+    target_names: list[str]
+    
+INFINITE_BACKLOG_MAPPING = ColumnMapping(
+    source_names=[
+        "IGDB ID", 
+        "Game name", 
+        "Game release date",
+        "Platform",
+        "Status",
+        "Completion",
+        "Playtime",
+        "Rating (Score)",
+        "Tags",
+        "Date added",
+        "Last updated"
+    ],
+    target_names=[
+        "IGDB ID",
+        "Game Name",
+        "Game Release Date",
+        "Platform",
+        "Status",
+        "Completion",
+        "Playtime",
+        "Rating",
+        "Tags",
+        "Date Added",
+        "Last Updated"
+    ]
+)
---
+++ tests/data/test_mappings.py
@@ -0,0 +1,30 @@
+import dataclasses
+import pytest
+
+from data import mappings
+
+class TestColumnMapping:
+    def test_contains_source_names(self):
+        new_mapping = mappings.ColumnMapping(
+            source_names=["Source"], 
+            target_names=["Target"])
+        
+        assert new_mapping.source_names == ["Source"]
+    
+    def test_contains_target_names(self):
+        new_mapping = mappings.ColumnMapping(
+            source_names=["Source"], 
+            target_names=["Target"])
+
+        assert new_mapping.target_names == ["Target"]
+
+    def test_assigning_after_initialization_throws_exception(self):
+        new_mapping = mappings.ColumnMapping(
+            source_names=["Source"], 
+            target_names=["Target"])
+        with pytest.raises(dataclasses.FrozenInstanceError) as exception_info:
+            new_mapping.source_names = ["Uh Oh"] # pyright: ignore[reportAttributeAccessIssue]
+            
+        assert isinstance(
+            exception_info.value, 
+            dataclasses.FrozenInstanceError)
